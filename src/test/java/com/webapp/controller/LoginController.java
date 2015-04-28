f5b34ea7f7e222503f745c9d17ca4674ac6415a1
sdpark <sanguc33@gmail.com>
2015-04-28 09:25:15 +0900

    폼

--- src/test/java/com/webapp/controller/LoginController.java
+++ src/test/java/com/webapp/controller/LoginController.java
@@ -1,12 +1,54 @@
 package com.webapp.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.webapp.model.User;
+
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
 
+	static Log log = LogFactory.getLog(LoginController.class);
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login(){
+		log.info("####################################");
+		log.info("#########GET#####");
+		log.info("####################################");
+		
 		return "login/loginform";
 	}
 	
-	public String login(User user){
-		return "login/success";
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(User user, HttpSession seeion){
+		
+		log.info("####################################");
+		log.info("#########POST##### id = "+user.getId()+" password = "+user.getPassword());
+		log.info("####################################");
+		
+		if ("webapp".equals(user.getId()) && "1234".equals(user.getPassword())){
+			//login Seccess
+			seeion.setAttribute("user", user);
+			return "login/success";
+		}else {
+			//login Fail
+			return "redirect:login";
+		}
+		
+
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String logout(HttpSession seeion){
+		
+		seeion.invalidate();
+		return "login/logoutsuccess";
 	}
 }
